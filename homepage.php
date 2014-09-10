4408e8c6d561e7cbb0a4f78e3df36bec86ae3c99
Ranga Mapuvire <avanna23@gmail.com>
2014-09-09 22:56:39 -0400

    changed headline

--- homepage.php
+++ homepage.php
@@ -7,6 +7,7 @@
 get_header(); ?>
 
 <p>Welcome to impact pharmacy</p>
+<p>Welcome to impact pharmacy online</p>
 
 <?php echo do_shortcode('[featured_products per_page="1" columns="1"]'); ?>
 <?php echo do_shortcode('[recent_products per_page="1" columns="1"]'); ?>
